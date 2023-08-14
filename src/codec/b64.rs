fa85f2aacf4dff6d51fff714c85c9ef72853b521
zed tan <zed@shootbird.work>
2023-08-15 00:00:01 +0200

    rewrite codec/b64 to remove bad architecture
    - remove EncodingAdapter trait -- the point of having a trait like this
      is so that we can apply it to generic implementations.
      however, all we've seen done to this is applying it to very specific
      implementations of encoders and decoders -- i.e. whatever the
      EncodingAdapter trait asks us to implement, it doesn't decouple
      it from the logic of encoding/decoding between formats.
      we could probaly do it, but right now this implementation of the
      adapter pattern is not working.
    - remove Base64 struct -- we don't need to store state here. base64
      encoders and decoders don't need to store state.
      we don't even need to store the b64<->ascii offsets, because
      the offsets are not implemented in the same way even between b64
      decode/encode and hex decode/encode -- both map very differently to
      ascii.
      - important difference 1: b64 uses '=' as a padding character, which
        is outside of its 6-bit space.
      - important difference 2: hex is case insensitive, b64 is case
        sensitive.

--- src/codec/b64.rs
+++ src/codec/b64.rs
@@ -1,101 +1,89 @@
-use super::adapter::EncodingAdapter;
-
-struct Base64;
 const UPPERCASEOFFSET: i8 = b'A' as i8; // b'A' is 65 in utf-8, but 0 in Base64. So the offset is b'A'-0.
 const LOWERCASEOFFSET: i8 = b'a' as i8 - 26; // b'a' is 97 in utf-8, but represents 26 in Base64. So the offset is b'a'-26=71.
 const DIGITOFFSET: i8 = b'0' as i8 - 52; // b'0' is 48 in utf-8, and represents 0 in Base64 (haha). So the offset is b'0'-52=-4
 const PADDING: char = '=';
 
-impl EncodingAdapter for Base64 {
-    fn value_to_char(&self, v: u8) -> Option<char> {
-        let v = v as i8;
-        let ascii_value = match v {
-            0..=25 => v + UPPERCASEOFFSET,
-            26..=51 => v + LOWERCASEOFFSET,
-            52..=61 => v + DIGITOFFSET,
-            62 => 43, // +
-            63 => 47, // -
+fn value_to_char(v: u8) -> Option<u8> {
+    let v = v as i8;
+    let ascii_value = match v {
+        0..=25 => v + UPPERCASEOFFSET,
+        26..=51 => v + LOWERCASEOFFSET,
+        52..=61 => v + DIGITOFFSET,
+        62 => 43, // +
+        63 => 47, // -
 
-            _ => return None,
-        } as u8;
+        _ => return None,
+    } as u8;
 
-        Some(ascii_value as char)
-    }
-    fn char_to_value(&self, c: char) -> Option<u8> {
-        //https://base64.guru/learn/base64-characters
-        let c = c as i8;
-        let base64_index = match c {
-            65..=90 => c - UPPERCASEOFFSET,
-            97..=127 => c - LOWERCASEOFFSET,
-            48..=57 => c - DIGITOFFSET,
-            43 => 62, // '+'
-            47 => 63, // '/'
+    Some(ascii_value)
+}
 
-            _ => return None,
-        } as u8;
+fn char_to_value(c: u8) -> Option<u8> {
+    //https://base64.guru/learn/base64-characters
+    let c = c as i8;
+    let base64_index = match c {
+        65..=90 => c - UPPERCASEOFFSET,
+        97..=127 => c - LOWERCASEOFFSET,
+        48..=57 => c - DIGITOFFSET,
+        43 => 62, // '+'
+        47 => 63, // '/'
 
-        Some(base64_index)
-    }
+        _ => return None,
+    } as u8;
+
+    Some(base64_index)
 }
 
 /* Attempt to perform bitwise operations to convert a 3 byte chunk from
 ascii to base64.
 */
-fn split_bits(chunk: &[u8]) -> Vec<u8> {
+fn encode_raw_chunk(chunk: &[u8]) -> Vec<u8> {
     match chunk.len() {
-      1 => vec![ // If chunk size is 1 byte, return 2 bytes
-        &chunk[0] >> 2, // shifts bits of the first byte 2 bits to the right. Effectively truncates the last two bits of the byte.
-        (&chunk[0] & 0b00000011) << 4, // selects the last 2 bits, and shifts them 4 bits left.
-      ],
-      2 => vec![
-        &chunk[0] >> 2,
-        (&chunk[0] & 0b00000011) << 4 | &chunk[1] >> 4, // Set the second byte by performing an inclusive OR between the first and second bytes
-        (&chunk[1] & 0b00001111) << 2, // For the second byte, shift the last 4 bits 2 bits to the left
-      ],
-      3 => vec![
-        &chunk[0] >> 2,
-        (&chunk[0] & 0b00000011) << 4 | &chunk[1] >> 4, // Set the second byte by performing an inclusive OR between the first and second bytes
-        (&chunk[1] & 0b00001111) << 2 | &chunk[2] >> 6,
-        &chunk[2] & 0b00111111, // select only the first 6 bits of the 3rd byte
-      ],
-      _ => unreachable!(),
+        1 => vec![
+            // If chunk size is 1 byte, return 2 bytes
+            &chunk[0] >> 2, // shifts bits of the first byte 2 bits to the right. Effectively truncates the last two bits of the byte.
+            (&chunk[0] & 0b00000011) << 4, // selects the last 2 bits, and shifts them 4 bits left.
+        ],
+        2 => vec![
+            &chunk[0] >> 2,
+            (&chunk[0] & 0b00000011) << 4 | &chunk[1] >> 4, // Set the second byte by performing an inclusive OR between the first and second bytes
+            (&chunk[1] & 0b00001111) << 2, // For the second byte, shift the last 4 bits 2 bits to the left
+        ],
+        3 => vec![
+            &chunk[0] >> 2,
+            (&chunk[0] & 0b00000011) << 4 | &chunk[1] >> 4, // Set the second byte by performing an inclusive OR between the first and second bytes
+            (&chunk[1] & 0b00001111) << 2 | &chunk[2] >> 6,
+            &chunk[2] & 0b00111111, // select only the first 6 bits of the 3rd byte
+        ],
+        _ => unreachable!(),
     }
 }
 
-fn encode_chunk<T: EncodingAdapter>(alphabet: &T, chunk: Vec<u8>) -> Vec<char> {
-  let mut out = vec![PADDING; 4];
+fn encode_chunk(chunk: Vec<u8>) -> Vec<u8> {
+    let mut out = vec![PADDING; 4];
 
-  for i in 0..chunk.len() {
-    if let Some(c) = alphabet.value_to_char(chunk[i]) {
-      //println!("Replacing {:?} with {:?}", out[i], c as char);
-      out[i] = c as char;
+    for i in 0..chunk.len() {
+        if let Some(c) = value_to_char(chunk[i]) {
+            //println!("Replacing {:?} with {:?}", out[i], c as char);
+            out[i] = c as char;
+        }
     }
-  }
-  out
+    out.iter().map(|c| *c as u8).collect::<Vec<u8>>()
 }
 
 pub fn encode_to_string(data: &[u8]) -> String {
-  //let classic_alphabet = &Bridge {};
-  encode(data).iter().map(|v| v.to_string()).collect::<String>()
+    //let classic_alphabet = &Bridge {};
+    encode(data)
+        .iter()
+        .map(|v| (*v as char).to_string())
+        .collect::<String>()
 }
 
-pub fn encode(data: &[u8]) -> Vec<char> {
-    let bridge = &Base64 {};
-    let chunked = data.chunks(3);
-
-    let mut bitshifted = Vec::new();
-    for c in chunked {
-      bitshifted.push(split_bits(c));
-    };
-
-    let mut out = Vec::new();
-    for b in bitshifted {
-      for c in encode_chunk(bridge, b) {
-        out.push(c)
-      };
-    };
-
-    out
+pub fn encode(data: &[u8]) -> Vec<u8> {
+    data.chunks(3)
+        .map(|c| encode_raw_chunk(c))
+        .flat_map(|c| encode_chunk(c))
+        .collect::<Vec<u8>>()
 }
 
 #[cfg(test)]
