132e75831655ae5fdf80b62f6cbcc6c592b63285
zed tan <zed@shootbird.work>
2023-08-16 19:24:15 +0200

    add method and test for b64_to_hex decode/encode

--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -5,6 +5,13 @@ pub mod hex;
 use super::codec::adapter::Codec;
 
 pub fn hex_to_b64_string(data: &str) -> String {
-    let b64_codec = &b64::Base64Adapter {};
-    b64_codec.encode_to_string(&hex::Hexadecimal{}.decode(data.as_bytes()).as_slice())
+    b64::Base64Adapter {}.encode_to_string(
+        &hex::Hexadecimal {}.decode(data.as_bytes()
+    ).as_slice())
+}
+
+pub fn b64_to_hex_string(data: &str) -> String {
+    hex::Hexadecimal {}.encode_to_string(
+        &b64::Base64Adapter {}.decode(data.as_bytes()
+    ).as_slice())
 }
--- tests/test_codecs.rs
+++ tests/test_codecs.rs
@@ -13,3 +13,16 @@ fn test_hex_to_b64() {
 
     assert_eq!(res, case.1);
 }
+
+#[test]
+fn test_b64_to_hex() {
+    let case = (
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+    );
+    
+    let res = b64_to_hex_string(case.0);
+    //let expected: Vec<char> = case.1.chars().collect();
+
+    assert_eq!(res, case.1);
+}
