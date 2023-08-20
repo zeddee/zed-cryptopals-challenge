a0c048e057f695e656d7378bbcadb949cef95abc
zed tan <zed@shootbird.work>
2023-08-20 13:11:42 +0200

    add introductory text to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! # Zed is working through Cryptopals Challenge
+//!
+//! Zed is working through <https://cryptopals.com/> to learn how to actually use rust.
+//!
+//! - Challenges are solved by writing and running tests in relevant modules.
+//! They're not solved or addressed directly.
+//! For example, you can't find a _crate::challenges::set1::challenge3_ module that directly addresses <https://cryptopals.com/sets/1/challenges/3>.
+//! - That's because the Cryptopals challenges build upon each other to form a comprehensive cryptographic toolkit.
+//! - Except: _Some_ challenges that result in one-off code _are_ addressed direcctly in [crate::challenges].
+
 pub mod challenges;
 pub mod codec;
 pub mod crack;
