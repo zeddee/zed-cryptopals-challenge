1ed8a2b789db1b360189fa12e3ff9913485d8a99
zed tan <zed@shootbird.work>
2023-08-20 14:11:16 +0200

    add references to docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,11 @@
 //! For example, you can't find a _crate::challenges::set1::challenge3_ module that directly addresses <https://cryptopals.com/sets/1/challenges/3>.
 //! - That's because the Cryptopals challenges build upon each other to form a comprehensive cryptographic toolkit.
 //! - Except: _Some_ challenges that result in one-off code _are_ addressed direcctly in [crate::challenges].
+//!
+//! ## References
+//!
+//! - https://dev.to/tiemen/implementing-base64-from-scratch-in-rust-kb1
+//! - [RFC 4648](https://www.rfc-editor.org/rfc/rfc4648#section-3.3)
 
 /// Implementation of various encryption and decryption tasks
 pub mod crack;
