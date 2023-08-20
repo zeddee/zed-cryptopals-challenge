7ff9bc8661fc0a0d0076797261954e9dc97dff5a
zed tan <zed@shootbird.work>
2023-08-20 12:32:06 +0200

    move tests for codec out of tests dir into the rust module

--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -23,3 +23,35 @@ pub fn hex_to_b64_string(data: &str) -> String {
 pub fn b64_to_hex_string(data: &str) -> String {
     hex::Hexadecimal {}.encode_to_string(&b64::Base64Adapter {}.decode(data.as_bytes()).as_slice())
 }
+
+#[cfg(test)]
+
+mod tests {
+
+    use super::*;
+    #[test]
+    fn test_hex_to_b64() {
+        let case = (
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+    );
+
+        let res = hex_to_b64_string(case.0);
+        //let expected: Vec<char> = case.1.chars().collect();
+
+        assert_eq!(res, case.1);
+    }
+
+    #[test]
+    fn test_b64_to_hex() {
+        let case = (
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+    );
+
+        let res = b64_to_hex_string(case.0);
+        //let expected: Vec<char> = case.1.chars().collect();
+
+        assert_eq!(res, case.1);
+    }
+}
